bc74243acb9bc851d9c9cdae856efb52f41cc03f
Зверев Александр <zverev@5042.ru>
2026-04-29 09:34:12 +0300

    feat: enable WASM mesher by default

--- src/defaultOptions.ts
+++ src/defaultOptions.ts
@@ -40,7 +40,7 @@ export const defaultOptions = {
   dayCycleAndLighting: true,
   loadPlayerSkins: true,
   renderEars: true,
-  wasmExperimentalMesher: false,
+  wasmExperimentalMesher: true,
   lowMemoryMode: false,
   starfieldRendering: true,
   defaultSkybox: true,
