06aac0372e7bc2fa68457335a078e0ca88b8d0ff
Vitaly Turovsky <vital2580@icloud.com>
2026-05-31 07:06:52 +0300

    should fix

--- scripts/downloadSoundsMap.mjs
+++ scripts/downloadSoundsMap.mjs
@@ -3,7 +3,7 @@ import fs from 'fs'
 const url = 'https://github.com/zardoy/prismarine-web-client/raw/sounds-generated/sounds.js'
 const savePath = 'dist/sounds.js'
 fetch(url).then(res => res.text()).then(data => {
-  fs.writeFileSync(savePath, data, 'utf8')
+  // fs.writeFileSync(savePath, data, 'utf8')
   if (fs.existsSync('.vercel/output/static/')) {
     fs.writeFileSync('.vercel/output/static/sounds.js', data, 'utf8')
   }
