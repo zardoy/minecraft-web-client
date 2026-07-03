d4f207a5f15c6aeb29a2177d90876ab32c90b469
dangdinhbaohoang12 <hoang.s297312@ilamail.edu.vn>
2026-07-03 21:39:54 +0700

    Update ChatProvider.tsx

--- src/react/ChatProvider.tsx
+++ src/react/ChatProvider.tsx
@@ -152,7 +152,7 @@ const ChatProviderBase = () => {
       })
     })
   }, [])
-  const disabledReason = disconnectedCleanup ?.wasConnected === false ? 'You have been disconnected from the server on ' + new Date(disconnectedCleanup.date).toLocaleString() : undefined;
+  const disabledReason = disconnectedCleanup ?.wasConnected === false ? 'You have been disconnected from the server on ' + new Date(disconnectedCleanup.date).toLocaleString() : undefined
   return <>
     <Chat
       chatVanillaRestrictions={chatVanillaRestrictions}
