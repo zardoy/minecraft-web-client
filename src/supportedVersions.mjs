418c14704acacc3186220783daf9d84d337147a3
Vitaly Turovsky <vital2580@icloud.com>
2026-07-06 11:55:16 +0300

    up minecraft-data to latest

--- src/supportedVersions.mjs
+++ src/supportedVersions.mjs
@@ -1,7 +1,6 @@
 import { supportedVersions, postNettyVersionsByProtocolVersion } from 'minecraft-data'
 
-// export const ignoredVersionsRegex = /(^0\.30c$)|w|-pre|-rc/
-export const ignoredVersionsRegex = /^(?!1\.).*$/
+export const ignoredVersionsRegex = /(^0\.30c$)|w|-pre|-rc|-snapshot/
 
 /** @type {string[]} */
 const versionsFromProtocol = Object.values(postNettyVersionsByProtocolVersion.pc).flat().filter(x => !ignoredVersionsRegex.test(x.minecraftVersion)).map(x => x.minecraftVersion)
