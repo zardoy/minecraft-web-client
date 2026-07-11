87515ef03efe980ba372f927068b3e2842efa0f8
dangdinhbaohoang12 <hoang.s297312@ilamail.edu.vn>
2026-07-11 22:13:17 +0700

    Update mouse.ts

--- src/mineflayer/plugins/mouse.ts
+++ src/mineflayer/plugins/mouse.ts
@@ -47,7 +47,7 @@ function cursorBlockDisplay (bot: Bot) {
 export default (bot: Bot) => {
   bot.loadPlugin(createMouse({
     useMineflayerInteractMethods: false,
-  blockPlacePrediction: false,
+    blockPlacePrediction: false,
   }))
 
   domListeners(bot)
